1e620cf3c3f2ba72c58146aa05766650d4a7dd2b
Junior Ales <edilson.ales.jr@gmail.com>
2018-02-17 20:16:47 +0000

    Initial setup
    typescript, parcel, hyperapp, tslint and jsx

---
+++ index.tsx
@@ -0,0 +1,22 @@
+import message from './message';
+
+import { app, h } from 'hyperapp';
+
+const initialState = {
+  count: 0
+};
+
+const appActions = {
+  down: value => state => ({ count: state.count - value }),
+  up: value => state => ({ count: state.count + value })
+};
+
+const view = (state, actions) => (
+  <div>
+    <h1>{state.count}</h1>
+    <button onclick={() => actions.down(1)}>-</button>
+    <button onclick={() => actions.up(1)}>+</button>
+  </div>
+);
+
+app(initialState, appActions, view, document.body);
