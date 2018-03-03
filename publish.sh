2165d0c6d55a887936ef80cdf217e9fcbe6e4afa
Junior Ales <edilson.ales.jr@gmail.com>
2018-03-03 17:26:59 +0000

    create publish script

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/Projects/connect-four
+yarn prod
+cp ./dist/* ~/Projects/junior-ales.github.io/connect4/
+cd ~/Projects/junior-ales.github.io/connect4/
+git add .
+git cm "publish connect4 $1"
+git push origin master
