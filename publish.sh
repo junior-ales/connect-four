a580a981c97b1336b142d826470018fb7e409e7f
Junior Ales <edilson.ales.jr@gmail.com>
2018-03-03 18:12:59 +0000

    bump to v1.0.1

--- publish.sh
+++ publish.sh
@@ -5,5 +5,5 @@ yarn prod
 cp ./dist/* ~/Projects/junior-ales.github.io/connect4/
 cd ~/Projects/junior-ales.github.io/connect4/
 git add .
-git cm "publish connect4 $1"
+git cm "publish connect4 v$1"
 git push origin master
